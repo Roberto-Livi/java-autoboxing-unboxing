27c07d3fddd8742b5478834d2d72a7a1758305dd
Roberto-Livi <robertomlivi@outlook.com>
2020-10-25 17:48:20 -0400

    autoboxing and unboxing using Double

--- autoboxing-unboxing/src/com/company/Main.java
+++ autoboxing-unboxing/src/com/company/Main.java
@@ -36,6 +36,7 @@ public class Main {
         Integer integer = new Integer(54);
         Double doubleValue = new Double(12.25);
 
+        // Autoboxing and Unboxing using Integer
         ArrayList<Integer> intArrayList = new ArrayList<Integer>();
         for(int i = 0; i <= 10; i++) {
             // Autoboxing
@@ -46,5 +47,21 @@ public class Main {
         for(int i = 0; i <= 10; i++) {
             System.out.println(i + " ----> " + intArrayList.get(i).intValue());
         }
+
+        Integer myIntValue = 56; // compile time: Integer.valueOf(56);
+        int myInt = myIntValue; // myIntValue.intValue();
+
+        // Autoboxing and Unboxing with Double
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for(double dbl = 0.0; dbl < 10.0; dbl += 0.5) {
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+
+        for(int i = 0; i< myDoubleValues.size(); i++) {
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " ---> " + value);
+        }
+
+
     }
 }
