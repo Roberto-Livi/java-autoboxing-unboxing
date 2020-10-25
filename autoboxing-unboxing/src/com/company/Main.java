db626f3e37e4c8400376d9c954fb62304bf79616
Roberto-Livi <robertomlivi@outlook.com>
2020-10-25 17:32:14 -0400

    applied autoboxing on first interation and outboxing on second iteration

--- autoboxing-unboxing/src/com/company/Main.java
+++ autoboxing-unboxing/src/com/company/Main.java
@@ -1,12 +1,50 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+class IntClass {
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
 
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<String>();
+        strArrayList.add("Tim");
+
+        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(54));
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(12.25);
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        for(int i = 0; i <= 10; i++) {
+            // Autoboxing
+            // Converting an integer, a base primitive type int to an Integer
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for(int i = 0; i <= 10; i++) {
+            System.out.println(i + " ----> " + intArrayList.get(i).intValue());
+        }
     }
 }
